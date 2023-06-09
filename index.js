8295f9194b1fb34b5b57e34953b3f5214f0813a8
Peter <pschoenbauer@gmail.com>
2023-06-09 14:40:46 -0400

    fetch lab hurray

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+function submitData (){
+    const formData = {
+        name: "Steve",
+        email: "steve@steve.com",
+      };
+      
+      const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify(formData),
+      };
+      
+      return fetch("http://localhost:3000/users", configurationObject)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (object) {
+          console.log(object.id);
+          let body = document.querySelector ("body");
+          body.append(object.id)
+        })
+        .catch(function (error) {
+          alert("He cogido un error!");
+          console.log(error.message);
+          let body = document.querySelector ("body");
+          body.append(error.message)
+        }); 
+}
